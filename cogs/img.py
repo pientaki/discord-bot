a15d24fe24397b49461fb61ccd14ed12dd64d46c
sou7878 <so.kyo2004@gmail.com>
2023-01-04 00:15:12 +0900

    エラー修正

--- cogs/img.py
+++ cogs/img.py
@@ -2,26 +2,11 @@ import os
 import discord
 from discord.ext import commands
 from discord import app_commands
-import cv2
 import requests
 import shutil
 from PIL import Image, ImageDraw , ImageFilter
 from io import BytesIO
 
-HAAR_FILE = "./data/haarcascade_frontalface_default.xml"
-IMG_PATH = "./img/"
-IMG_SIZE = (128, 128)
-
-def mosaic(img, scale=0.1):
-    h, w = img.shape[:2] 
-
-    dst = cv2.resize(
-        img, dsize=None, fx=scale, fy=scale, interpolation=cv2.INTER_NEAREST
-    )
-
-    dst = cv2.resize(dst, dsize=(w, h), interpolation=cv2.INTER_NEAREST)
-
-    return dst
 
 class Image(commands.Cog):
     def __init__(self, bot: commands.Bot):        
@@ -90,38 +75,6 @@ class Image(commands.Cog):
         await ctx.send(file=discord.File("./img/blur.gif"))
         os.remove("./img/blur.gif")
 
-    @image.command(name="mosaic",with_app_command = True, description="画像から人の顔を検出してモザイクをかけます")
-    @app_commands.rename(shashin="画像")    
-    @app_commands.describe(shashin="対応フォーマットは png jpg です")
-    async def facecut(self, ctx: commands.Context, shashin: discord.Attachment):
-        await ctx.defer()
-
-        cascade = cv2.CascadeClassifier(HAAR_FILE)
-
-        data = shashin.url
-
-        file_name = data.rsplit("/", 1)[1]
-        rst = requests.get(data, stream = True)
-        
-        open_file = open(IMG_PATH + file_name, "wb")
-
-        shutil.copyfileobj(rst.raw, open_file)
-        open_file.close()
-    
-        img = cv2.imread(IMG_PATH + file_name, cv2.IMREAD_COLOR)
-    
-        face_list = cascade.detectMultiScale(img, scaleFactor = 1.1, minNeighbors=1, minSize=(1, 1))
-        
-        for x, y, w, h in face_list:
-            roi = img[y : y + h, x : x + w]
-            roi[:] = mosaic(roi)
-        
-
-        cv2.imwrite('./img/face_rectangle.jpg', img)
-
-        await ctx.send(file=discord.File("./img/face_rectangle.jpg"))
-        os.remove(IMG_PATH + file_name)
-        os.remove("./img/face_rectangle.jpg")
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Image(bot))
