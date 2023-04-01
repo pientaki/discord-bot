ce0dc28334a7565eec07a8c7c963094a4f6b81a6
pientaki <so.kyo2004@gmail.com>
2023-04-01 14:28:24 +0900

    fix

--- cogs/function.py
+++ cogs/function.py
@@ -198,30 +198,34 @@ class Search(commands.Cog):
         else:
             await ctx.send("場所が見つかりません")
 
-    #@commands.hybrid_command(name="covid", description="大阪府の新型コロナウイルス新規感染者数を表示します", with_app_command=True)    
-    #async def covid(self, ctx: commands.Context):
-        #await ctx.defer()
-        #async with ctx.typing():
-            #options = webdriver.ChromeOptions()
-            #options.add_argument('--headless')
-            #options.add_argument('--disable-gpu')
-            #options.add_argument('--no-sandbox')
-            #options.add_argument('--disable-dev-shm-usage')
-            #options.add_argument('--remote-debugging-port=9222')
-            #options.add_experimental_option('excludeSwitches', ['enable-logging'])
-            #browser = webdriver.Chrome("chromedriver.exe", options=options)
-            #browser.set_window_size(950, 800)
-           
-            #browser.get('https://www.watch.impress.co.jp/extra/covid19/?pref=27') 
-            #data = browser.find_elements_by_class_name("extra-wrap")
-            #text = data[0].text
-            #time.sleep(5)
-            #browser.quit()
+    '''
 
-            #embed=discord.Embed(title="大阪府新型コロナウイルス感染者数", color=discord.Color.from_rgb(255, 0, 0))
-            #embed.set_thumbnail(url="https://www.apsf.org/wp-content/uploads/newsletters/2020/3502/coronavirus-covid-19.png")
-            #embed.description=(text)
-            #await ctx.send(embed=embed)
+    @commands.hybrid_command(name="covid", description="大阪府の新型コロナウイルス新規感染者数を表示します", with_app_command=True)    
+    async def covid(self, ctx: commands.Context):
+        await ctx.defer()
+        async with ctx.typing():
+            options = webdriver.ChromeOptions()
+            options.add_argument('--headless')
+            options.add_argument('--disable-gpu')
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
+            options.add_argument('--remote-debugging-port=9222')
+            options.add_experimental_option('excludeSwitches', ['enable-logging'])
+            browser = webdriver.Chrome("chromedriver.exe", options=options)
+            browser.set_window_size(950, 800)
+           
+            browser.get('https://www.watch.impress.co.jp/extra/covid19/?pref=27') 
+            data = browser.find_elements_by_class_name("extra-wrap")
+            text = data[0].text
+            time.sleep(5)
+            browser.quit()
+
+            embed=discord.Embed(title="大阪府新型コロナウイルス感染者数", color=discord.Color.from_rgb(255, 0, 0))
+            embed.set_thumbnail(url="https://www.apsf.org/wp-content/uploads/newsletters/2020/3502/coronavirus-covid-19.png")
+            embed.description=(text)
+            await ctx.send(embed=embed)
+
+    '''
 
     @commands.hybrid_command(name="embed", description="埋め込みメッセージを作成します", with_app_command=True)
     @app_commands.rename(title="タイトル", description="内容")    
